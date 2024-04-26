dfff3b251574c89e0caa9e827fbc1257d556961b
icaro9293 <125396165+icaro9293@users.noreply.github.com>
2024-04-26 10:29:03 -0400

    módulo-pasta

---
+++ Javascript/Módulo-Login/script.js
@@ -0,0 +1,32 @@
+import { Login } from "./módulo.js";
+import { CaixaMSG } from './Javascript/Classe-CaixaMensagem/módulo.js'
+
+const configCaixaErro = {
+    cor: 'red',
+    titulo: 'erro',
+    texto: 'usuário ou senha incorretos',
+    tipo: 'ok'
+}
+
+const configCaixaOk = {
+    cor: 'lightblue',
+    titulo: 'login',
+    texto: 'login efetuado com sucesso',
+    tipo: 'ok'
+}
+
+const callbackOk = () => {
+    CaixaMSG.mostarCaixa(configCaixaOk)
+}
+const callbackNok = () => {
+    CaixaMSG.mostarCaixa(configCaixaErro)
+}
+
+const configLogin = {
+    cor: 'red',
+    img: 'lixeira.png'
+}
+Login.login(callbackOk, callbackNok, configLogin)
+
+
+
