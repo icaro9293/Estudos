7e58cfbc154873b21f4a706ae29b51d0d6455ef6
icaro9293 <125396165+icaro9293@users.noreply.github.com>
2024-07-11 10:31:44 -0400

    att

--- ESTOQUE/backoffice/colaboradores/colaboradores.js
+++ ESTOQUE/backoffice/colaboradores/colaboradores.js
@@ -8,17 +8,16 @@ const tiposColab = document.getElementById('itipo')
 const divTel = document.getElementById('telefones')
 const campoTel = document.getElementById('itel')
 const campoNome = document.getElementById('inome')
-const campoTipo = document.getElementById('itipo')
+// const campoTipo = document.getElementById('itipo')
 const campoStatus = document.getElementById('istatus')
 const btnAddTel = document.getElementById('addTel')
 const campoFoto = document.getElementById('ifoto')
 const imagem = document.getElementById('imagem')
 
+let modoJanela = 'novo'
 
-
-
-btnAddTel.addEventListener('click', (evt) => {
-    const valorDigitado = campoTel.value
+const criarModeloTel = (Tel, idtel) => {
+    const valorDigitado = Tel //campoTel.value
     const div = document.createElement('div')
     div.setAttribute('class', 'tel')
     div.innerHTML = valorDigitado
@@ -27,13 +26,26 @@ btnAddTel.addEventListener('click', (evt) => {
     const lixeira = document.createElement('img')
     lixeira.setAttribute('src', '../../imgs/lixeira.svg')
     lixeira.setAttribute('class', 'delTel')
+    lixeira.setAttribute('data-idtel', idtel)
     divTel.appendChild(lixeira)
     lixeira.addEventListener('click', (evt) => {
         const alvo1 = evt.target.previousElementSibling
         const alvo2 = evt.target
-        alvo1.remove()
-        alvo2.remove()
+        const idAlvo = alvo2.dataset.idtel
+        const endpointDelTel = `http://127.0.0.1:1880/deltelefone/${idAlvo}`
+        fetch(endpointDelTel)
+            .then(res => {
+                if (res.status == 200) {
+                    alvo1.remove()
+                    alvo2.remove()
+
+                }
+            })
     })
+}
+
+btnAddTel.addEventListener('click', (evt) => {
+    criarModeloTel(campoTel.value)
     campoTel.focus()
     campoTel.value = ''
 })
@@ -60,13 +72,13 @@ fetch(endpointGetAll)
             const c3 = document.createElement('div')
             c3.setAttribute('class', 'colunaDados c3')
             c3.innerHTML = e.n_id_tipousuario
-            if (c3.innerHTML == '1') {
-                c3.innerHTML += ': Super usuário'
-            } else if (c3.innerHTML == '2') {
-                c3.innerHTML += ': Administrador'
-            } else if (c3.innerHTML == '3') {
-                c3.innerHTML += ': Colaborador'
-            }
+            // if (c3.innerHTML == '1') {
+            //     c3.innerHTML += ': Super usuário'
+            // } else if (c3.innerHTML == '2') {
+            //     c3.innerHTML += ': Administrador'
+            // } else if (c3.innerHTML == '3') {
+            //     c3.innerHTML += ': Colaborador'
+            // }
             linha.appendChild(c3)
 
             const c4 = document.createElement('div')
@@ -76,14 +88,58 @@ fetch(endpointGetAll)
 
             const c5 = document.createElement('div')
             c5.setAttribute('class', 'colunaDados c5')
-            c5.innerHTML = 'C R U D'
             linha.appendChild(c5)
+
+            const imgStatus = document.createElement('img')
+            imgStatus.setAttribute('src', '../../imgs/toggleOn.svg')
+            imgStatus.setAttribute('class', 'imgFunções')
+            c5.appendChild(imgStatus)
+
+            const imgEdit = document.createElement('img')
+            imgEdit.setAttribute('src', '../../imgs/editar.svg')
+            imgEdit.setAttribute('class', 'imgFunções')
+            imgEdit.addEventListener('click', (evt) => {
+                modoJanela = 'editar'
+
+                const id = evt.target.parentNode.parentNode.firstElementChild.innerHTML
+                const endpointColabId = `http://127.0.0.1:1880/dadoscolab/${id}`
+                fetch(endpointColabId)
+                    .then(res => res.json())
+                    .then((res) => {
+                        console.log(res[0].n_id_tipousuario)
+                        document.querySelector('.topoJanela').innerHTML = 'Editar colaborador'
+                        janelaPopup.classList.remove('ocultar')
+                        campoNome.value = res[0].s_nome_usuario
+                        campoStatus.value = res[0].c_status_usuario
+                        tiposColab.value = res[0].n_id_tipousuario
+                        imagem.src = res[0].s_foto_usuario
+                    })
+                const endpointColabTelefone = `http://127.0.0.1:1880/telefonescolab/${id}`
+                fetch(endpointColabTelefone)
+                    .then(res => res.json())
+                    .then((res) => {
+                        divTel.innerHTML = ''
+                        res.forEach((tel) => {
+                            criarModeloTel(tel.s_numero_telefone, tel.n_id_telefone)
+
+                        })
+
+                    })
+            })
+            c5.appendChild(imgEdit)
+
+            const imgDeletar = document.createElement('img')
+            imgDeletar.setAttribute('src', '../../imgs/lixeira.svg')
+            imgDeletar.setAttribute('class', 'imgFunções')
+            c5.appendChild(imgDeletar)
 
             dados.appendChild(linha)
         })
     })
 
 btnCriar.addEventListener('click', (evt) => {
+    modoJanela = 'novo'
+    document.querySelector('.topoJanela').innerHTML = 'Novo cadastro'
     janelaPopup.classList.remove('ocultar')
     const endpointGetTipos = 'http://127.0.0.1:1880/listartipos'
     fetch(endpointGetTipos)
@@ -127,12 +183,19 @@ btnAdd.addEventListener('click', (evt) => {
         .then((res) => {
             if (res.status == 200) {
                 alert('colaborador inserido com sucesso')
+                campoNome.value = ''
+                campoTipo.value = ''
+                campoStatus.value = ''
+                campoFoto.value = ''
+                imagem.setAttribute('src', '#')
+                campoTel.value = ''
+                divTel.innerHTML = ''
             } else {
                 alert('erro ao inserir colaborador')
             }
         })
 
-    janelaPopup.classList.add('ocultar')
+    // janelaPopup.classList.add('ocultar')
 })
 
 const converterImagem_b64 = (localDestino, arquivoImg) => {
